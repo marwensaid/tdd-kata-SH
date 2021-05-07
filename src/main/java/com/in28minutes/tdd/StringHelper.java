0f1f5409410372c3b160ecd07c2f2b679ed31858
marwen.saidi <marwen.saidi@orange.com>
2021-05-07 11:43:28 +0200

    solution

--- src/main/java/com/in28minutes/tdd/StringHelper.java
+++ src/main/java/com/in28minutes/tdd/StringHelper.java
@@ -1,14 +1,23 @@
 package com.in28minutes.tdd;
 
+import java.awt.*;
+
 public class StringHelper {
 
-	// ABCD,BCD
-	public String truncateAInFirst2Positions(String input) {
-		return "";
-	}
 
-	public boolean areFirstAndLastTwoCharsTheSame(String input) {
+    // ABCD,BCD
+    public String truncateAInFirst2Positions(String input) {
+        return "";
+    }
 
-		return true;
-	}
+    public static boolean areFirstAndLastTwoCharsTheSame(String input) {
+
+        if (input.length() < 2){
+            return false;
+        }
+
+        String first2chars = input.substring(0, 2);
+        String last2chars = input.substring(input.length() - 2);
+        return first2chars.equals(last2chars);
+    }
 }
--- src/test/java/com/in28minutes/tdd/StringHelperTest.java
+++ src/test/java/com/in28minutes/tdd/StringHelperTest.java
@@ -1,6 +1,11 @@
 package com.in28minutes.tdd;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 
 public class StringHelperTest {
 
@@ -10,19 +15,30 @@ public class StringHelperTest {
 
 	@Test
 	public void testAreFirstAndLastTwoCharsTheSame_EmptyCondition() {
+		assertFalse(StringHelper.areFirstAndLastTwoCharsTheSame(""));
 	}
 
+	// "A"==> false
 	@Test
 	public void testAreFirstAndLastTwoCharsTheSame_StringWith1Char() {
+		assertFalse(StringHelper.areFirstAndLastTwoCharsTheSame("A"));
 	}
 
+	// "AB" ==> true
 	@Test
 	public void testAreFirstAndLastTwoCharsTheSame_TwoChars() {
+		assertTrue(StringHelper.areFirstAndLastTwoCharsTheSame("AB"));
 	}
 
 	// "A"=>false,"AB"=>true,"ABC"=>false,"AAA"=>true,"ABCAB"=>true,
 	// "ABCDEBA"=>false
 	@Test
 	public void testAreFirstAndLastTwoCharsTheSame_ThreeChars() {
+
+		assertTrue(StringHelper.areFirstAndLastTwoCharsTheSame("AAA"));
+		assertFalse(StringHelper.areFirstAndLastTwoCharsTheSame("ABA"));
+		assertTrue(StringHelper.areFirstAndLastTwoCharsTheSame("ABAB"));
+		assertFalse(StringHelper.areFirstAndLastTwoCharsTheSame("ABC"));
+
 	}
 }
